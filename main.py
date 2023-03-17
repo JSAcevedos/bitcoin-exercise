6ece62c38f1415f06da3e1e4591c2e326eb87a17
Juan Acevedo <juan.acevedo9805@gmail.com>
2023-03-16 23:36:04 -0500

    Add comments to the code

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+# Variables declaration and created input to get the data
 prices = input().split()
 days = []
 index = 0
 
+# Loop that compares each item with the next items until find a bigger price
+
 for i in prices:
+# Local variables for the loop, index will allow only search after each list item, convert each item in integer to make the comparations, set the days counter to zero for each item
     index += 1
     price = int(i)
     day = 0
+# Count how many days musto to wait to find a better price, if there is no a better price set day to zero
     for j in range(len(prices) - index):
         if price >= int(prices[index + j]):
             day += 1
@@ -16,6 +21,7 @@ for i in prices:
             day = 0
     days.append(day)
 
+# Print the array output without the brackets or the commas.
 for i in range(len(days)):
     if i == len(days) - 1:
         print(days[i], end="")
